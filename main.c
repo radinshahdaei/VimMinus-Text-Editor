d759c606dbbd1cf150793b7627650fe3ac939ff5
radinshahdaei <radin.shahdaei@gmail.com>
2023-01-16 18:21:36 +0330

    improved correction

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include "correction.h"
 
 char string[100] = {};
 void clear() // clear string
@@ -12,70 +13,70 @@ void clear() // clear string
         string[i] = '\0';
     }
 }
-void remq(char string[]) // remove quotation
-{
-    int len1 = strlen(string);
-    for (int i = 0; i < len1 - 1; i++)
-    {
-        string[i] = string[i + 1];
-    }
-    string[len1 - 2] = '\0';
-    string[len1 - 1] = '\0';
-}
 
-void createfile() // createfile
+void createfile(char *string) // createfile
 {
     char buff[100] = {};
-
-    char *add = "/Users/Radin/School/Programming/project-radinshahdaei"; // current directory address
-    char address[200];
-    int addlen = strlen(add);
-
-    scanf("%[^\n]", string); // string input
     int len = strlen(string);
 
-    if (string[0] == '"' && string[len - 1] == '"') // check for quotes
+    for (int i = 1; i < len; i++) // creating directory
     {
-        printf("1\n");
-        remq(string);
-        len = strlen(string);
+        buff[i - 1] = string[i];
+        if (string[i + 1] == '/')
+        {
+            mkdir(buff, 0777);
+        }
     }
 
-    if (string[0] == '"' ^ string[len - 1] == '"' == 1) // check for error
+    FILE *file; // check file
+    if (fopen(buff, "r") == 0)
     {
-        printf("Invalid input.\n");
+        file = fopen(buff, "w");
     }
-
     else
     {
-        for (int i = 1; i < len; i++) // creating directory
+        printf("File already exists.\n");
+    }
+    fclose(file);
+}
+
+void insert(char directory[], char string[], int line, int start)
+{
+    char *add = "/Users/Radin/School/Programming/project-radinshahdaei";
+    int addlen = strlen(add);
+    int len = strlen(directory);
+    char address[200];
+    for (int i = 0; i < addlen; i++) // creating address for file
+    {
+        address[i] = add[i];
+    }
+    for (int i = 0; i < len; i++)
+    {
+        address[addlen + i] = directory[i];
+    }
+
+    if (fopen(address, "r") == 0)
+    {
+        printf("File does not exist.\n");
+    }
+    else
+    {
+        FILE *file;
+        file = fopen(address, "w");
+        int counter1 = 1;
+        int counter2 = 0;
+        char c;
+        while (counter1 != line && counter2 != start)
         {
-            buff[i - 1] = string[i];
-            if (string[i + 1] == '/')
+            c = fgetc(file);
+            counter2++;
+            if (c == '\n')
             {
-                mkdir(buff, 0777);
+                counter1++;
+                counter2 = 0;
             }
         }
-
-        for (int i = 0; i < addlen; i++) // creating address for file
-        {
-            address[i] = add[i];
-        }
-        for (int i = 0; i < len; i++)
-        {
-            address[addlen + i] = string[i];
-        }
-
-        FILE *file; // check file
-        if (fopen(address, "r") == 0)
-        {
-            file = fopen(address, "w");
-        }
-        else
-        {
-            printf("File already exists.\n");
-        }
-        fclose(file);
+        fputs(string, file);
     }
 }
 
@@ -94,7 +95,10 @@ int main()
             if (strcmp(string, "-f") == 0)
             {
                 getchar();
-                createfile();
+
+                clear();
+                scanf("%[^\n]", string);
+                createfile(corstr(string));
             }
             else
             {
